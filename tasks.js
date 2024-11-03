ef74e024f0797220bac9f17e3cf1074352c0d290
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-03 13:12:17 +0200

    Refactored createBookElement function to use custom 'book-preview' element instead of creating HTML button element

--- tasks.js
+++ tasks.js
@@ -1,4 +1,5 @@
-import { authors } from './data.js';
+//import { authors } from './data.js';
+import './custom/book-preview.js'
 
 /**
  * Creates an HTML button element representing a book.
@@ -12,16 +13,11 @@ import { authors } from './data.js';
 
 // Function to create an HTML element for a book
 export function createBookElement({ author, id, image, title }) {
-    const bookElement = document.createElement('button');
-    bookElement.classList = 'preview';
-    bookElement.setAttribute('data-preview', id);
-    bookElement.innerHTML = `
-        <img class="preview__image" src="${image}" />
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `;
+    const bookElement = document.createElement('book-preview');
+    bookElement.setAttribute('author', author);
+    bookElement.setAttribute('id', id);
+    bookElement.setAttribute('image', image);
+    bookElement.setAttribute('title', title);
     return bookElement;
 }
 
